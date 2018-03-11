5ae32de5f7cecaa689689172e6966e22d56c4fc7
Chase <chaseit12@gmail.com>
2018-03-11 21:39:12 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,7 +14,7 @@ class Song
   end
   
   
-    def self.new_by_filename(filename)
+  def self.new_by_filename(filename)
     artist, song = filename.split(" - ")
     new_song = self.new(song)
     new_song.artist_name = artist
