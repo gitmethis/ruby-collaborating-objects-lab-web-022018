67965b6d08f525ee597c00ab7ce4a7fa6b3a5fa6
Chase <chaseit12@gmail.com>
2018-03-11 21:38:47 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,7 +16,9 @@ class Song
   
   def self.new_by_filename(filename)
       new_song = Song.new(filename)  
+      new_song 
       new_song
+      
   end
   
   def self.artist_name=(artist_name)
